fa4ba95bec8f32a12176c7df438c04a93a394e50
zhe-he <he.zhe@kuyun.com>
2020-11-20 18:35:30 +0800

    add vuex install

--- src/store/KyVuex.js
+++ src/store/KyVuex.js
@@ -1,9 +1,19 @@
+let KyVue;
 // 实现Store类
 class Store {}
 
 // 实现插件
-function install() {
+function install(Vue) {
+    KyVue = Vue
 
+    // 混入
+    Vue.mixin({
+        beforeCreate() {
+            if (this.$options.store) {
+                Vue.prototype.$store = this.$options.store;
+            }
+        }
+    })
 }
 
 export default { Store, install }
